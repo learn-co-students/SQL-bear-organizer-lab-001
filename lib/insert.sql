09363ccb1d1593e3c845d305e3a796e3a427f508
fhernandez173 <f.hernandez173@gmail.com>
2016-09-06 04:10:33 -0400

    SQL practice

---
+++ lib/insert.sql
@@ -0,0 +1,79 @@
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Mr. CHocolate",
+    4,
+    "M",
+    "Brown",
+    "Calm",
+    1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Rowdy",
+    7,
+    "F",
+    "Blue",
+    "Mean",
+    1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Tabitha",
+    12,
+    "F",
+    "Green",
+    "Shy",
+    0
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Sergeant Brown",
+    21,
+    "F",
+    "Pink",
+    "Tough",
+    1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Melissa",
+    16,
+    "F",
+    "Green",
+    "Shy",
+    0
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Grinch",
+    14,
+    "M",
+    "Red",
+    "Grumpy",
+    1
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    "Wendy",
+    17,
+    "F",
+    "Yellow",
+    "aggressive",
+    0
+);
+
+INSERT INTO bears (
+  name, age, gender, color, temperament, alive) VALUES (
+    NULL,
+    21,
+    "F",
+    "Yellow",
+    "Defensive",
+    0
+);
